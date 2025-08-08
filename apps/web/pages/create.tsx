11313cbfa0d470e87fb55d70c09c4def63160e2d
imattau <imatt.au@protonmail.com>
2025-08-08 15:20:52 +1000

    feat: initialize ffmpeg on create page

--- apps/web/pages/create.tsx
+++ apps/web/pages/create.tsx
@@ -1,7 +1,16 @@
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 
+const ffmpeg = createFFmpeg({ log: true })
 const CreatorWizard = dynamic(() => import('@/components/create/CreatorWizard'), { ssr: false })
 
 export default function CreatePage() {
+  useEffect(() => {
+    ffmpeg.load()
+  }, [])
+
   return <CreatorWizard />
 }
+
+export { ffmpeg, fetchFile }
