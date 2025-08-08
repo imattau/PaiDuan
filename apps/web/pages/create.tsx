a63002f63e7d4e36a2dbf7bb869fc6f15c594b3b
imattau <imatt.au@protonmail.com>
2025-08-08 16:10:13 +1000

    refactor ffmpeg loading

--- apps/web/pages/create.tsx
+++ apps/web/pages/create.tsx
@@ -1,16 +1,13 @@
 import dynamic from 'next/dynamic'
 import { useEffect } from 'react'
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
-
-const ffmpeg = createFFmpeg({ log: true })
+import { loadFFmpeg } from '@/utils/ffmpeg'
 const CreatorWizard = dynamic(() => import('@/components/create/CreatorWizard'), { ssr: false })
 
 export default function CreatePage() {
   useEffect(() => {
-    ffmpeg.load()
+    loadFFmpeg()
   }, [])
 
   return <CreatorWizard />
 }
 
-export { ffmpeg, fetchFile }
