767593e9679fde7d821056587024015bd018ed6e
imattau <imatt.au@protonmail.com>
2025-08-09 09:40:16 +1000

    refactor: use AppShell in settings layout

--- apps/web/components/SettingsLayout.tsx
+++ apps/web/components/SettingsLayout.tsx
@@ -1,11 +1,8 @@
+import AppShell from '@/components/layout/AppShell';
 import SideNav from './SideNav';
+
 export default function SettingsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <SideNav />
-      <div className="lg:ml-48 pt-6 pb-24 flex justify-center">
-        <div className="w-full max-w-[640px] space-y-8 px-4">{children}</div>
-      </div>
-    </>
+    <AppShell left={<SideNav />} center={<div className="space-y-8">{children}</div>} right={<></>} />
   );
 }
