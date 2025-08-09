062be6d22d5132b967a0f9777c3cf54dfeff8233
imattau <imatt.au@protonmail.com>
2025-08-10 09:55:46 +1000

    feat(web): make root layout a client component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import '../styles/globals.css';
 import Providers from './providers';
 import { LayoutProvider } from '@/context/LayoutContext';
+import { useParams } from 'next/navigation';
 
 export default function RootLayout({
   children,
-  params: { locale },
 }: {
   children: React.ReactNode;
-  params: { locale?: string };
 }) {
+  const { locale } = useParams<{ locale?: string }>();
+
   return (
     <html lang={locale}>
       <body>
