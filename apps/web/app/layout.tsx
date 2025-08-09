e4202ef886f66dd16c7f58a0d5dd2e169a6c76cc
imattau <imatt.au@protonmail.com>
2025-08-10 08:56:24 +1000

    Wrap app with NextIntl provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,36 @@
 import '../styles/globals.css';
 import Providers from './providers';
 import { LayoutProvider } from '@/context/LayoutContext';
+import { NextIntlClientProvider } from 'next-intl';
+import { locales } from '../utils/locales';
+import { notFound } from 'next/navigation';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  if (!locales.includes(locale as typeof locales[number])) {
+    notFound();
+  }
+
+  const messages = {
+    common: (await import(`../locales/${locale}/common.json`)).default,
+  };
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body>
         <LayoutProvider>
-          <Providers>{children}</Providers>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <Providers>{children}</Providers>
+          </NextIntlClientProvider>
         </LayoutProvider>
       </body>
     </html>
