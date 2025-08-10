d85582e3300edab278dd6b69497be8cb529be8e9
imattau <imatt.au@protonmail.com>
2025-08-10 22:08:20 +1000

    feat(web): unify theming

--- apps/web/styles/theme-config.ts
+++ apps/web/styles/theme-config.ts
@@ -2,7 +2,6 @@ import type { ThemeConfig } from '@chakra-ui/react';
 
 const themeConfig: ThemeConfig = {
   initialColorMode: 'light',
-  useSystemColorMode: true,
 };
 
 export default themeConfig;
